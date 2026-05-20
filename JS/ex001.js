b3abd2868cb4a7037b71f418c23b43ce10e70077
GuilhermeCruzzz790 <guilhermedealmeida452@gmail.com>
2026-05-19 21:22:05 -0300

    Aprendendo operadores

---
+++ JS/ex001.js
@@ -0,0 +1,11 @@
+// let nota1 = 10
+// let nota2 = 8
+
+// let media = (nota1 + nota2) /2
+// console.log("Sua média foi:", media )
+
+// let teste = "100"
+// console.log(typeof teste)
+
+let comparação = 10 == "10"
+console.log(comparacao)
